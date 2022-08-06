4187e63836d9926be4e1b8dbb704ceee903dda6e
DMITRY ULASAU <dmitrushok@gmail.com>
2022-08-06 12:57:26 -0400

    REGISTER FORM

--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
+
+
+@auth.route('/login')
+def login():
+    return render_template('login.html', text="Testing")
+
+@auth.route('/logout')
+def logout():
+    return "<p>LOGOUT</p>"
+
+@auth.route('/register')
+def sign_up():
+    return render_template('register.html')
